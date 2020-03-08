4a994f60b02718a2c931efb3d9cd25981eb0b64d
0x4bd0 <abdoumjr@gmail.com>
2020-03-08 18:21:51 +0100

    seting up the db

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const magic = express()
+const mongoose = require('mongoose')
 require('dotenv').config()
 
+
+mongoose.connect(process.env.DB_URLN{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+})
 magic.set('view engine','ejs')
 
 magic.use(express.static(__dirname + '/public'));
