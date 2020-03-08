04d89a5d054ff33f113e5a32de7792e06a10ba36
0x4bd0 <abdoumjr@gmail.com>
2020-03-08 18:40:16 +0100

    routing stuff

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const magic = express()
+const router = express.Router()
 const mongoose = require('mongoose')
 const su = require('./models/urls')
+const URL =require('./controllers/main')
 require('dotenv').config()
 
+router.use(URL);
 
 mongoose.connect(process.env.DB_URL,{
     useNewUrlParser : true,
