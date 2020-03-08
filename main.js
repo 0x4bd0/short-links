d39465ebe58a7391cb8a2edca7b367dc70e93e61
0x4bd0 <abdoumjr@gmail.com>
2020-03-08 18:09:20 +0100

    fixing annoying css

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 
 magic.set('view engine','ejs')
 
+magic.use(express.static(__dirname + '/public'));
+
 magic.listen(process.env.PORT,()=>{
     console.log('🏃 up and running 🏃')
 })
